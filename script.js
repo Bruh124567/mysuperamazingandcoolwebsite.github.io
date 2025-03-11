04aba1f2c89c514d869eee34acc6309fb5f366bd
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-11 10:54:01 +0100

    megint csináltam vmit

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.createElement('button');
+button.textContent = 'Click me!';
+button.addEventListener('click', function() {
+  alert('Button clicked!');
+});
+document.body.appendChild(button);
